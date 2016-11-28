8d31d7c19ecb30bf63c3eb81dbea029808e175f4
Anna Jolly <jolly.anna@ymail.com>
2016-11-28 15:21:49 -0500

    merging new login stuff

--- database/signup.py
+++ database/signup.py
@@ -1,53 +1,99 @@
 #!/usr/bin/python
 
-import json, sys, sqlite3
+import json
+import cgi
+import sqlite3
+import sys
+
+json = cgi.FieldStorage()
+
+#print('Content-type: text/html\n\n')
 
 print "Content-Type: text/plain;charset=utf-8"
 print
 
-#data = sys.stdin.read()
+if json.getvalue('sponsor') == "y":
+	sponsor = 1
+else: sponsor = 0
+if json.getvalue('port') == "y":
+	port = 1
+else: port = 0
+if json.getvalue('github') == "y":
+	github = 1
+else: github = 0
+if json.getvalue('lnkdin') == "y":
+	lnkdin = 1
+else: lnkdin = 0
 
-data = '{"username": "bill", "password": "pass1234", "cgpa": "3.94", "mgpa": "3.81", "grad": "3", "school": "McGill University", "major": "Computer Engineering", "sponsor": "yes", "port": "no", "github": "yes", "lnkdin": "no", "interns": "2", "educ": "Bachelors", "L1": "4", "L2": 3, "L3": 1, "L4": 1, "L5": 0, "M1": 12, "M2": 9, "M3": 2, "M4": 1, "M5": 1, "S1": 6, "S2": 4, "S3": 4, "S4": 3, "S5": 2}'
-
-j = json.loads(data)
-
-if j['sponsor'] == "y":
-	j['sponsor'] = 1
-else: j['sponsor'] = 0
-if j['port'] == "y":
-	j['port'] = 1
-else: j['port'] = 0
-if j['github'] == "y":
-	j['github'] = 1
-else: j['github'] = 0
-if j['lnkdin'] == "y":
-	j['lnkdin'] = 1
-else: j['lnkdin'] = 0
-
-
-conn = sqlite3.connect("members.db")
-
-cursor = conn.execute("SELECT * from MEMBERS WHERE UNAME=(?);", [j['username']])
-if len(cursor.fetchall()) > 0:
-	print "username taken"
-
-else:
-	cur = conn.cursor()
-	cur.execute("INSERT INTO MEMBERS (UNAME, PASSWD, CGPA, MGPA, GRAD, SCHOOL, MAJOR, SPONSOR, PORT, GITHUB, LNKDIN, INTERNS, EDUC, L1, L2, L3, L4, L5, M1, M2, M3, M4, M5, S1, S2, S3, S4, S5)"
-		" VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", 
-		(j['username'], j['password'], j['cgpa'], j['mgpa'], j['grad'], j['school'], j['major'], j['sponsor'], j['port'], j['github'], j['lnkdin'], j['interns'], j['educ'], 
-			j['L1'], j['L2'], j['L3'], j['L4'], j['L5'], j['M1'], j['M2'], j['M3'], j['M4'], j['M5'], j['S1'], j['S2'], j['S3'], j['S4'], j['S5']))
-	conn.commit()
-	print "success"
-
- 
-# f = open("user.log", "a")
-# f.write(j['name'] + "\n")
-# f.close()
-
- #{"username": "test3", "password": "pass1234", "cgpa": 2.68, "mgpa": 3.02, "grad": 5, "school": "UBC", "major": "Computer Science", "sponsor": 1, "port": 0, "github": 0, "lnkdin", 1, "educ": "Bachelors", "L1": 32, "L2": 2, "L3": 2, "L4": 1, "L5": 0, "M1": 16, "M2": 2, "M3": 0, "M4":0, "M5": 0, "S1": 13, "S2": 6, "S3": 4, "S4": 2, "S5": 1}
-
-
-
-conn.close()
-
+try:
+    fileName = "./user.log"
+    ref = open(fileName, "a")
+    ref.write(json.getvalue('username'))
+    ref.write("\n")
+    ref.write(json.getvalue('cgpa'))
+    ref.write("\n")
+    ref.write(json.getvalue('mgpa'))
+    ref.write("\n")
+    ref.write(json.getvalue('grad'))
+    ref.write("\n")
+    ref.write(json.getvalue('school'))
+    ref.write("\n")
+    ref.write(json.getvalue('major'))
+    ref.write("\n")
+    ref.write(json.getvalue('interns'))
+    ref.write("\n")
+    ref.write(json.getvalue('educ'))
+    ref.write("\n")
+    ref.write(str(sponsor))
+    ref.write("\n")
+    ref.write(str(port))
+    ref.write("\n")
+    ref.write(str(github))
+    ref.write("\n")
+    ref.write(str(lnkdin))
+    ref.write("\n")
+    ref.write(json.getvalue('L1'))
+    ref.write("\n")
+    ref.write(json.getvalue('L2'))
+    ref.write("\n")
+    ref.write(json.getvalue('L3'))
+    ref.write("\n")
+    ref.write(json.getvalue('L4'))
+    ref.write("\n")
+    ref.write(json.getvalue('L5'))
+    ref.write("\n")
+    ref.write(json.getvalue('M1'))
+    ref.write("\n")
+    ref.write(json.getvalue('M2'))
+    ref.write("\n")
+    ref.write(json.getvalue('M3'))
+    ref.write("\n")
+    ref.write(json.getvalue('M4'))
+    ref.write("\n")
+    ref.write(json.getvalue('M5'))
+    ref.write("\n")
+    ref.write(json.getvalue('S1'))
+    ref.write("\n")
+    ref.write(json.getvalue('S2'))
+    ref.write("\n")
+    ref.write(json.getvalue('S3'))
+    ref.write("\n")
+    ref.write(json.getvalue('S4'))
+    ref.write("\n")
+    ref.write(json.getvalue('S5'))
+    ref.write("\n")
+    #conn = sqlite3.connect("database/members.db")
+    print "db opened\n"
+    #cur = conn.cursor()
+    # cur.execute("INSERT INTO MEMBERS (UNAME, PASSWD) VALUES ('usertest','passtest');")
+	print "execute"
+    #cur.execute("INSERT INTO MEMBERS (UNAME, PASSWD, CGPA, MGPA, GRAD, SCHOOL, MAJOR, SPONSOR, PORT, GITHUB, LNKDIN, INTERNS, EDUC, L1, L2, L3, L4, L5, M1, M2, M3, M4, M5, S1, S2, S3, S4, S5) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", (name, passwd, cgpa, mgpa, grad, school, major, sponsor, port, github, lnkdin, interns, educ, L1, L2, L3, L4, L5, M1, M2, M3, M4, M5, S1, S2, S3, S4, S5))
+    #cur.execute("INSERT INTO MEMBERS (UNAME, PASSWD) VALUES (?, ?);", (json.getvalue('username'), json.getvalue('password')))
+	#cur.execute("INSERT INTO MEMBERS (UNAME, PASSWD, CGPA, MGPA, GRAD, SCHOOL, MAJOR, SPONSOR, PORT, GITHUB, LNKDIN, INTERNS, EDUC, L1, L2, L3, L4, L5, M1, M2, M3, M4, M5, S1, S2, S3, S4, S5) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", (json.getvalue('username'), json.getvalue('password'), float(json.getvalue('cgpa')), float(json.getvalue('mgpa')), int(json.getvalue('grad')), json.getvalue('school'), json.getvalue('major'), sponsor, port, github, lnkdin, int(json.getvalue('interns')), json.getvalue('educ'), int(json.getvalue('L1')), int(json.getvalue('L2')), int(json.getvalue('L3')), int(json.getvalue('L4')), int(json.getvalue('L5')), int(json.getvalue('M1')), int(json.getvalue('M2')), int(json.getvalue('M3')), int(json.getvalue('M4')), int(json.getvalue('M5')), int(json.getvalue('S1')), int(json.getvalue('S2')), int(json.getvalue('S3')), int(json.getvalue('S4')), int(json.getvalue('S5'))))
+	#conn.commit()
+    print "success"
+except:
+    print "Unexpected error:", sys.exc_info()[0]
+    raise
+ref.close()
+#conn.close()
--- database/usernameAvailability.py
+++ database/usernameAvailability.py
@@ -7,22 +7,17 @@ import sys
 
 json = cgi.FieldStorage()
 
-#print('Content-type: text/html\n\n')
-
-print "Content-Type: text/plain;charset=utf-8"
-print
+print('Content-type: text/html\n\n')
 
 try:
     conn = sqlite3.connect("members.db")
     cursor = conn.execute("SELECT * from MEMBERS")
-    b = False
+    alreadyExists = False
     for row in cursor:
         if row[0] == json.getvalue('name'):
-            b = True
-            if row[1] == json.getvalue('pwd'):
-                print "success"
-            else: print "wrong password"
-    if not b: print "no such user"
+            alreadyExists = True
+            print "username already exists"
+    if not alreadyExists: print "username is available"
 except:
     print "Unexpected error:", sys.exc_info()[0]
     raise
