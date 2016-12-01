27a4d99d4bde6978add721b01418e1ec8550df8b
alexandercmzt <alexandercmzt@github.com>
2016-12-01 10:53:53 -0500

    backups should work now

--- database/demo.py
+++ database/demo.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python
-with open('dum.log', 'r') as f:
-	html = f.read()
+
+import json
+import cgi
+import sqlite3
+import sys
+
+json = cgi.FieldStorage()
 
 print "Content-Type: text/plain;charset=utf-8"
 print
+
+x=json.getvalue('xAxis')
+s=json.getvalue('filterSchool')
+
+if s != "":
+	filename = "backup_plots/" + x + "-mcgill.backup"
+else:
+	filename = "backup_plots/" + x + ".backup"
+
+with open(filename, 'r') as f:
+	html = f.read()
+
 print html
